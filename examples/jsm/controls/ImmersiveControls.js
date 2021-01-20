dd014f7062a91fe523e9df527a4d67279b85d380
Felix Mariotto <felixmariotto@gmail.com>
2021-01-20 12:27:27 +0100

    ImmersieControls: add arc with cursor to controllers

--- examples/jsm/controls/ImmersiveControls.js
+++ examples/jsm/controls/ImmersiveControls.js
@@ -1,8 +1,17 @@
 import {
 	EventDispatcher,
+	Color,
 	Vector2,
 	Vector3,
-	Group
+	Group,
+	Line,
+	Mesh,
+	LineBasicMaterial,
+	MeshBasicMaterial,
+	LatheBufferGeometry,
+	BufferGeometry,
+	BufferAttribute,
+	Raycaster
 } from '../../../build/three.module.js';
 import { XRControllerModelFactory } from '../webxr/XRControllerModelFactory.js';
 import { XRHandModelFactory } from '../webxr/XRHandModelFactory.js';
@@ -17,16 +26,24 @@ const ImmersiveControls = function ( camera, renderer ) {
 	this.enabled = true;
 	this.mouseMovePower = 0.45;
 	this.mouseClamp = 0.8;
+	this.arcControlPointDist = 1.5;
+	this.arcTension = 0.5;
 	this.axes = new Vector2();
+	this.arcColor = new Color( 0xff56f5 );
 	this.viewDirection = camera.getWorldDirection( new Vector3() );
+	this.arcMaterial = new LineBasicMaterial( { color: this.arcColor } );
 
 	// INTERNALS
 
 	const scope = this;
+	const arcVerts = 20;
+	const raycaster = new Raycaster();
 
 	const domElemSize = new Vector2();
 	const mouse = new Vector2();
-	const crossVec = new Vector3();
+	const vec1 = new Vector3();
+	const vec2 = new Vector3();
+	const vec3 = new Vector3();
 
 	const controllerModelFactory = new XRControllerModelFactory();
 
@@ -58,13 +75,124 @@ const ImmersiveControls = function ( camera, renderer ) {
 
 	// XR INPUT
 
-	const xrInputs = [];
+	this.xrInputs = [];
 
 	for ( let i=0 ; i<2 ; i++ ) {
 
 		const input = {};
 
-		xrInputs.push( input );
+		this.xrInputs.push( input );
+
+		// get controller spaces + add controller mesh
+
+		input.rayGroup = renderer.xr.getController( i );
+		input.gripGroup = renderer.xr.getControllerGrip( i );
+
+		input.gripGroup.add( controllerModelFactory.createControllerModel( input.gripGroup ) );
+
+		// arc pointer
+
+		input.arcGeometry = new BufferGeometry();
+		input.arc = new Line( input.arcGeometry, this.arcMaterial );
+		input.arcGeometry.setAttribute( 'position', new BufferAttribute( new Float32Array( 3 * arcVerts ), 3 ) );
+
+		input.arcVecOrg = new Vector3(); 
+		input.arcVecMid = new Vector3();
+		input.arcVecEnd = new Vector3();
+
+		const lathePoints = [ new Vector2( 0.4, 0 ), new Vector2( 0.3, 0 ) ];
+		const latheGeom = new LatheBufferGeometry( lathePoints, 32 );
+
+		input.arcCursor = new Mesh(
+			latheGeom,
+			new MeshBasicMaterial( { color: scope.arcColor } )
+		);
+
+		input.arcCursor.geometry.rotateX( Math.PI / 2 );
+
+		input.updateArc = function () {
+
+			if ( this.rayGroup ) this.rayGroup.add( input.arc );
+
+			// get handles of bezier curve
+
+			this.arcVecOrg.set( 0, 0, 0 );
+			this.arcVecMid.set( 0, 0, scope.arcControlPointDist * -1 );
+			this.arcVecEnd.set( 0, 0, scope.arcControlPointDist * -3 );
+
+			// move the end of the curve according to projection direction
+
+			this.rayGroup.localToWorld( this.arcVecOrg );
+			this.rayGroup.localToWorld( this.arcVecMid );
+			this.rayGroup.localToWorld( this.arcVecEnd );
+
+			const dotUp = vec1
+			.copy( this.arcVecMid )
+			.sub( this.arcVecOrg )
+			.normalize()
+			.dot( camera.up );
+
+			// we got the dot from up vector, we tween it with the user-defined ImmersiveControls.arcTension
+			// and reduce the end handle height proportionally with the middle handle length.
+			this.arcVecEnd.y -= ( scope.arcControlPointDist * 5 ) * Math.pow( dotUp * 0.5 + 0.5, scope.arcTension * 3 + 1 );
+
+			raycaster.ray.origin.copy( this.arcVecMid );
+			raycaster.ray.direction
+			.copy( this.arcVecEnd )
+			.sub( this.arcVecMid )
+			.normalize();
+
+			this.rayGroup.worldToLocal( this.arcVecOrg );
+			this.rayGroup.worldToLocal( this.arcVecMid );
+			this.rayGroup.worldToLocal( this.arcVecEnd );
+
+			// update geometry and cast rays
+
+			const posAttrib = this.arcGeometry.attributes.position;
+
+			let firstIntersect;
+
+			for ( let i=0 ; i<arcVerts ; i++ ) {
+
+				// bezier curve sampling to update arc geometry
+
+				const a = i / ( arcVerts - 1 );
+				vec1.lerpVectors( this.arcVecOrg, this.arcVecMid, a );
+				vec2.lerpVectors( this.arcVecMid, this.arcVecEnd, a );
+				vec3.lerpVectors( vec1, vec2, a );
+
+				posAttrib.setXYZ( i, vec3.x, vec3.y, vec3.z );
+
+			}
+
+			posAttrib.needsUpdate = true;
+
+			// find intersection
+
+			const intersects = raycaster.intersectObject( scope.navigationObject, true );
+
+			if ( intersects.length ) {
+
+				scope.viewerSpace.add( this.arcCursor );
+
+				this.arcCursor.position.copy( intersects[0].point );
+				this.arcCursor.position.addScaledVector( intersects[0].face.normal, 0.02 );
+				scope.viewerSpace.worldToLocal( this.arcCursor.position );
+
+				vec1.copy( intersects[0].point );
+				vec1.add( intersects[0].face.normal );
+
+				this.arcCursor.lookAt( vec1 );
+
+			} else {
+
+				scope.viewerSpace.remove( this.arcCursor );
+
+			}	
+
+		}
+
+		// vr controller setup
 
 		input.handleEvent = function ( event, eventName ) {
 
@@ -109,11 +237,6 @@ const ImmersiveControls = function ( camera, renderer ) {
 			this.handedness = null;
 
 		}
-
-		input.rayGroup = renderer.xr.getController( i );
-		input.gripGroup = renderer.xr.getControllerGrip( i );
-
-		input.gripGroup.add( controllerModelFactory.createControllerModel( input.gripGroup ) );
 
 		input.rayGroup.addEventListener( 'connected', (e) => {
 
@@ -180,6 +303,19 @@ const ImmersiveControls = function ( camera, renderer ) {
 				} );
 
 				changedBtns.forEach( (changedBtn) => {
+
+					// we check that the pressed button is not the primary controller button,
+					// because this button must only fire select events.
+
+					// https://immersive-web.github.io/webxr-gamepads-module/#xr-standard-gamepad-mapping
+					if (
+						this.gamepad.mapping === "xr-standard" &&
+						changedBtn === this.gamepad.buttons[0]
+					) {
+						return
+					}
+
+					// fire keyup or keydown event
 
 					if ( changedBtn.pressed ) {
 
@@ -401,15 +537,13 @@ const ImmersiveControls = function ( camera, renderer ) {
 		this.viewerSpace.add( camera );
 		camera.position.y = 1.5;
 
-		xrInputs.forEach( (input) => {
+		this.xrInputs.forEach( (input) => {
 
 			this.viewerSpace.add( input.rayGroup, input.gripGroup );
 
 		} );
 
 	}
-
-	this.setViewerSpace( new Group() );
 
 	this.handleResize = function () {
 
@@ -422,7 +556,7 @@ const ImmersiveControls = function ( camera, renderer ) {
 
 	this.update = function ( deltaTime ) {
 
-		xrInputs.forEach( input => input.checkState() );
+		this.xrInputs.forEach( input => input.checkState() );
 
 		if ( xrSession ) {
 
@@ -436,14 +570,14 @@ const ImmersiveControls = function ( camera, renderer ) {
 
 			// update viewDirection according to mouse position on dom element
 
-			crossVec.crossVectors( this.viewDirection, camera.up );
-			crossVec.normalize();
+			vec1.crossVectors( this.viewDirection, camera.up );
+			vec1.normalize();
 
-			this.viewDirection.addScaledVector( crossVec, Math.pow( mouse.x, 3 ) * this.mouseMovePower * 0.1 );
+			this.viewDirection.addScaledVector( vec1, Math.pow( mouse.x, 3 ) * this.mouseMovePower * 0.1 );
 
-			crossVec.crossVectors( crossVec, this.viewDirection );
+			vec1.crossVectors( vec1, this.viewDirection );
 
-			this.viewDirection.addScaledVector( crossVec, Math.pow( mouse.y, 3 ) * this.mouseMovePower * 0.1 );
+			this.viewDirection.addScaledVector( vec1, Math.pow( mouse.y, 3 ) * this.mouseMovePower * 0.1 );
 
 			this.viewDirection.normalize();
 
@@ -465,10 +599,19 @@ const ImmersiveControls = function ( camera, renderer ) {
 
 		}
 
+		//
+
+		this.xrInputs.forEach( ( xrInput ) => {
+
+			xrInput.updateArc();
+
+		} );
+
 	}
 
 	//
 
+	this.setViewerSpace( new Group() );
 	this.handleResize();
 
 };
